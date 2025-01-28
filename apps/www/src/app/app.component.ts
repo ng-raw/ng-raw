8dc29af57015cdf3227e02a30465167a2ec017ab
Ayoub KHIAL <ayouub.khial@gmail.com>
2025-01-28 03:27:02 +0100

    chore: initial commit - setup nx monorepo

---
+++ apps/www/src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  imports: [RouterOutlet],
+  selector: 'app-root',
+  templateUrl: './app.component.html'
+})
+export class AppComponent {
+  title = 'ng-raw';
+}
